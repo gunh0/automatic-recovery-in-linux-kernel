49eeb037dfbdcc870794099eecfd5db4c0e0b731
devgunho <devgun@kakao.com>
2020-08-28 15:46:15 +0900

    ADD file copy & ERR file copy - reboot...

--- auto_recovery_lkm/file_handle.c
+++ auto_recovery_lkm/file_handle.c
@@ -2,7 +2,7 @@
 
 void file_handle(char *filepath)
 {
-    struct file *filp;
+    struct file *filp, *backup_fp;
     unsigned char buf[BUF_SIZE];
     int ret;
     int line_counter = 0;
@@ -10,9 +10,8 @@ void file_handle(char *filepath)
     struct timeval time;
     struct rtc_time tm;
     unsigned long local_time;
-    char backup_path_str[BUF_SIZE];
     char temp_str[50];
-
+    char backup_path_str[BUF_SIZE];
     unsigned int iter;
 
     /* kernel memory access setting */
@@ -61,30 +60,43 @@ void file_handle(char *filepath)
 
         printk("-[*] file name state : [%s]\n", backup_path_str);
 
-        // printk("[*] print offset(h) | [%s]\n", filepath);
-        // printk(" 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\n");
-        // printk(" -----------------------------------------------\n");
-
-        ret = vfs_read(filp, buf, 1, &filp->f_pos);
-        while (ret != 0)
+        /* copy origin file */
+        backup_fp = filp_open(backup_path_str, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+        if(IS_ERR(backup_fp))
         {
-            /* (struct file *file, char __user *buf, size_t count, loff_t *offset) */
-            /* Reads count bytes from the file offset position. */
-            /* This action updates the file offset (*offset). */
-            // printk(" %02X", buf[0]);
-            ret = vfs_read(filp, buf, 1, &filp->f_pos);
+            printk("-[-] backup file error\n");
+        }
+        else
+        {
+            // printk("[*] print offset(h) | [%s]\n", filepath);
+            // printk(" 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\n");
+            // printk(" -----------------------------------------------\n");
 
-            if (line_counter >= 15)
+            ret = vfs_read(filp, buf, 1, &filp->f_pos);
+            while (ret != 0)
             {
-                line_counter = 0;
-                // printk("\n");
+                /* (struct file *file, char __user *buf, size_t count, loff_t *offset) */
+                /* Reads count bytes from the file offset position. */
+                /* This action updates the file offset (*offset). */
+                // printk(" %02X", buf[0]);
+                vfs_write(backup_fp,buf,1,&backup_fp->f_pos);
+                ret = vfs_read(filp, buf, 1, &filp->f_pos);
+
+                if (line_counter >= 15)
+                {
+                    line_counter = 0;
+                    // printk("\n");
+                }
+                else
+                    line_counter++;
             }
-            else
-                line_counter++;
         }
     }
     printk("\n");
     filp_close(filp, NULL);
-    /* restore kernel memory setting */
+    filp_close(backup_fp, NULL);
+    
+    /* Restore kernel memory setting */
+    /* Lock the kernel memory permission */
     set_fs(old_fs);
 }
